b3f7689e704bf1d5e40923404f99b1c5b72d6ab0
Raja <66917909+omsaibandari@users.noreply.github.com>
2025-04-25 13:01:11 +0530

    Update main.js

--- Reactjs course practice/main.js
+++ Reactjs course practice/main.js
@@ -1,20 +1,35 @@
-let a=false;
-let b= false;
+// let a=false;
+// let b= false;
 
-console.log("and:",a&&b);
+// console.log("and:",a&&b);
 
-console.log("or:",a||b);
+// console.log("or:",a||b);
 
-function employeeName(){
-    return getMyname(a && getMyname("Omsai"));
-}
+// function employeeName(){
+//     return getMyname(a && getMyname("Omsai"));
+// }
 
-function getMyname(S){
-    return "Hello ", S;
-}
+// function getMyname(S){
+//     return "Hello ", S;
+// }
 
-console.log("The Employee name is:",employeeName());
+// console.log("The Employee name is:",employeeName());
 
-console.log(a && getMyname("Omsai")); //you will get the output only if the a is true.
+// console.log(a && getMyname("Omsai")); //you will get the output only if the a is true.
+
+//short-hand notation
+// let name1="xyz";
+// let id=1;
+// let rating= 5;
+
+// const product = {
+//     name1,
+//     id,
+//     rating
+// }
+// console.log(product);
+
+//getting the list of products from the external links
+
 
 
